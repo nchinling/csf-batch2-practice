964e241be3ffe5ba7d74d2bd4158ed521046f0d7
nchinling <nchinling@gmail.com>
2023-05-30 19:50:32 +0800

    working form

--- csf-assessment-practice-client/src/app/app.module.ts
+++ csf-assessment-practice-client/src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { DisplayComponent } from './components/display.component';
+import { ListComponent } from './components/list.component';
+import { HttpClientModule } from '@angular/common/http'
+import { UploadService } from './upload.service';
+
+const appRoutes: Routes = [
+  { path: '', component: ListComponent, title: 'Photo Storage' },
+  { path: 'form', component: FormComponent, title: 'Form'},
+  { path: 'display', component: DisplayComponent, title: 'Display'},
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule, HttpClientModule,
+    RouterModule.forRoot(appRoutes, { useHash: true})
   ],
-  providers: [],
+  providers: [UploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ csf-assessment-practice-client/src/app/components/form.component.ts
@@ -0,0 +1,52 @@
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
+import { UploadService } from '../upload.service';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit{
+
+  form!: FormGroup
+  fb=inject(FormBuilder)
+  uploadSvc = inject(UploadService)
+
+  @ViewChild('uploadFile')
+  uploadFile!: ElementRef
+
+  ngOnInit(): void{
+    this.form = this.fb.group({
+      name: this.fb.control<string>('', [Validators.required] ),
+      title: this.fb.control<string>('', [Validators.required]),
+      comments: this.fb.control<string>(''),
+      file: this.fb.control<File | null>(null, [Validators.required])
+    })
+
+  }
+
+  upload(){
+    const data = this.form.value
+    const f: File = this.uploadFile.nativeElement.files[0]
+    console.info('>>> data: ', data)
+    console.info('>>> file: ', f)
+
+    //use promise
+    firstValueFrom(this.uploadSvc.upload(data['title'], f))
+      .then(result=>{
+        alert('uploaded')
+        this.form.reset
+      })
+      .catch(err =>{
+        alert(JSON.stringify(err))
+      })
+  }
+
+
+
+
+
+
+}
